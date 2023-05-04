7d9485e8b4ac6d419564773281c1fe67abd7c5d3
Guino-Elvis <ginoyujra38@gmail.com>
2023-05-04 10:07:41 -0500

    este siii

--- pruevamicroservidor_gino_web/microservicios/catalogo/src/main/java/com/upeu/catalogo/catalogo/repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.upeu.catalogo.catalogo.repository;
-
-import com.upeu.catalogo.catalogo.entity.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto,Integer> {
-
-}
--- pruevamicroservidor_gino_web/microservicios_2023_1/venta/src/main/java/com/example/venta/repository/VentaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.venta.repository;
-
-import com.example.venta.entity.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VentaRepository extends JpaRepository<Venta, Integer> {
-}
